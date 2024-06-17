d841c2a3931cf6640f61105c0f097f1f191cd76c
Akihiro Chida <95008746+a-chida@users.noreply.github.com>
2024-06-17 06:10:52 +0000

    feat: 🎸 create CloudFront and S3 stack

--- lib/cdk-static-website-stack.ts
+++ lib/cdk-static-website-stack.ts
@@ -1,16 +1,87 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 
 export class CdkStaticWebsiteStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // S3 bucket
+    const s3Bucket = new s3.Bucket(this, "S3Bucket", {
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      bucketName: "cdk-static-website-origin",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStaticWebsiteQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // CloudFront
+    const origin = new origins.S3Origin(s3Bucket);
+    const distribution = new cloudfront.Distribution(this, "distribution", {
+      defaultBehavior: { origin: origin },
+      defaultRootObject: "index.html",
+      geoRestriction: cloudfront.GeoRestriction.allowlist("JP"),
+      httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
+      minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_200,
+    });
+
+    // Use L1 constructor to implement OAC because L2 constructor is not support Origin Access Control
+    // Ref: https://github.com/aws/aws-cdk/issues/21771
+    const cfnOriginAccessControl = new cloudfront.CfnOriginAccessControl(
+      this,
+      "cf-origin-access-control",
+      {
+        originAccessControlConfig: {
+          name: s3Bucket.bucketRegionalDomainName,
+          originAccessControlOriginType: "s3",
+          signingBehavior: "always",
+          signingProtocol: "sigv4",
+          description: "S3 Access Control",
+        },
+      }
+    );
+
+    // Additional settings for origin 0 (0: s3Bucket)
+    const cfnDistribution = distribution.node
+      .defaultChild as cloudfront.CfnDistribution;
+    // Delete OAI
+    cfnDistribution.addOverride(
+      "Properties.DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity",
+      ""
+    );
+    // OAC does not require CustomOriginConfig
+    cfnDistribution.addPropertyDeletionOverride(
+      "DistributionConfig.Origins.0.CustomOriginConfig"
+    );
+    // By default, the s3 WebsiteURL is set and an error occurs, so set the S3 domain name
+    cfnDistribution.addPropertyOverride(
+      "DistributionConfig.Origins.0.DomainName",
+      s3Bucket.bucketRegionalDomainName
+    );
+
+    // OAC settings
+    cfnDistribution.addOverride(
+      "DistributionConfig.Origins.0.OriginAccessControlId",
+      cfnOriginAccessControl.getAtt("Id")
+    );
+
+    // add S3 bucket policy for CloudFront
+    s3Bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        sid: "AllowCloudFrontServicePrincipalReadOnly",
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
+        actions: ["s3:GetObject"],
+        resources: [s3Bucket.arnForObjects("*")],
+        conditions: {
+          StringEquals: {
+            "aws:SourceArn": `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
+          },
+        },
+      })
+    );
   }
 }
