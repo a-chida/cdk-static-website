05d9ab72c8bdec545cc97ccf592d31e35fa0852a
Akihiro Chida <95008746+a-chida@users.noreply.github.com>
2024-06-17 06:11:07 +0000

    test: 💍 create Stack test

--- test/cdk-static-website.test.ts
+++ test/cdk-static-website.test.ts
@@ -1,17 +1,42 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkStaticWebsite from '../lib/cdk-static-website-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as CdkStaticWebsite from "../lib/cdk-static-website-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-static-website-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkStaticWebsite.CdkStaticWebsiteStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe("CdkStaticWebsite", () => {
+  const app = new cdk.App();
+  const stack = new CdkStaticWebsite.CdkStaticWebsiteStack(app, "TestStack", {
+    env: { region: "us-east-1" },
+  });
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  // snapshot test
+  test("snapshot test", () => {
+    expect(template.toJSON()).toMatchSnapshot();
+  });
+
+  // S3 Bucket
+  test("S3 Bucket", () => {
+    template.hasResourceProperties("AWS::S3::Bucket", {
+      PublicAccessBlockConfiguration: {
+        BlockPublicAcls: true,
+        BlockPublicPolicy: true,
+        IgnorePublicAcls: true,
+        RestrictPublicBuckets: true,
+      },
+    });
+  });
+
+  // CloudFront Distribution
+  test("CloudFront Distribution", () => {
+    template.findResources("AWS::CloudFront::Distribution");
+  });
+
+  // CloudFront OriginAccessControl
+  test("CloudFront OriginAccessControl", () => {
+    template.hasResourceProperties("AWS::CloudFront::OriginAccessControl", {
+      OriginAccessControlConfig: {
+        OriginAccessControlOriginType: "s3",
+      },
+    });
+  });
 });
